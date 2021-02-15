be3e42bacd3238ee0bdaf1d21ab93d063db30fa2
AdityaNG <adityang5@gmail.com>
2021-02-15 16:19:12 +0530

    Week 4

--- Session 04/p6.s
+++ Session 04/p6.s
@@ -2,20 +2,22 @@
 .TEXT
 LDR R0, =A
 
-
+MOV R7, #3
 MOV R3, #3
 
 LOOP_1:
 	LOOP_2:
 		MOV R4, #3
 
-		MLA R5, R3, #3, R4
+		MLA R5, R3, R7, R4
+		MUL R5, R5, #4
 	        LDR R6, [R0], R5
+
        		ADD R7, R7, R6
 
         
 		SUBS R4, R4, #1
-		BNE LOOP
+		BNE LOOP_2
 	SUBS R3, R3, #1
         BNE LOOP_1
 
